120ec0e6403e0ae44d8cec169a8b328d0142e5a6
114-10\user <nestor.s@mail.ru>
2019-10-08 20:00:00 +0300

    enters name

--- src/Classwork/lesson1/Man.java
+++ src/Classwork/lesson1/Man.java
@@ -1,7 +1,15 @@
 package Classwork.lesson1;
 
 public class Man {
+	public Man(int hairLength) {
+		this.hairLength = hairLength;
+	}
+
 	int hairLength;
+	
+	public Man() {
+		hairLength = 10;
+	}
 
 	void increaseHair() {
 		hairLength = hairLength + 1;
---
+++ src/Classwork/lesson2/Enter.java
@@ -0,0 +1,18 @@
+package Classwork.lesson2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Enter {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("input username: ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String userName = reader.readLine();
+
+		System.out.println("Hello " + userName);
+
+	}
+}
