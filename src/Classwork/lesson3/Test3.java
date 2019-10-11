d7c23f76fe01d4dc057f4f7fa5544ea8aab4c17a
114-10\user <nestor.s@mail.ru>
2019-10-11 21:43:34 +0300

    before collapse

---
+++ src/Classwork/lesson3/Test3.java
@@ -0,0 +1,13 @@
+package Classwork.lesson3;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Count is:" + i);
+		}
+
+	}
+
+}
---
+++ src/Classwork/lesson3/Test4.java
@@ -0,0 +1,18 @@
+package Classwork.lesson3;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+
+		int i = 17;
+		while (i <= 28) {
+			if ((i % 13) == 0) {
+				break;
+			}
+			i++;
+		}
+		System.out.println("First number is:" + i);
+
+	}
+
+}
