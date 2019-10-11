0f3c6db88d568fe073482f4be14596dcdeec1dbb
114-10\user <nestor.s@mail.ru>
2019-10-11 21:58:54 +0300

    arrays sort

---
+++ src/Classwork/lesson3/Test9.java
@@ -0,0 +1,18 @@
+package Classwork.lesson3;
+
+import java.util.Arrays;
+
+public class Test9 {
+
+	public static void main(String[] args) {
+
+		int[] array = { 2, 7, 6, 3, 0, 1};
+		Arrays.sort(array);
+
+		for (int element : array) {
+			System.out.println(element + " ");
+		}
+
+	}
+
+}
