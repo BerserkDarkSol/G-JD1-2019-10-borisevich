5653bbe4deca9f36ed9a775e683c6dbf2011ba35
borisevich yuri <berserksdarksol@gmail.com>
2019-10-18 18:03:26 +0300

    home tasks. 2.3-2.7

--- src/homework/practice1/task.java
+++ src/homework/practice1/task.java
@@ -9,9 +9,40 @@ public class task {
 //		String s = in.nextLine();
 //		System.out.println("You entered string " + s);
 
-		int n = 100;
-		int length = String.valueOf(n).length();
-		System.out.println(length);
+//		int array[] = {1, 2, 3};
+//
+//		for (int i = 0; i < array.length; i++) {
+//			System.out.println(i);
+//			for (int b = array[i]; b == 0; b = b / 10) {
+//				System.out.println("b = " + b % 10);
+//			}
+//		}
+
+//		int a = 14;
+//		int b = 3;
+//		int c = a % b;
+//		System.out.println(c);
+//		
+//		newArray();
+		int Ar[] = { 1, 2 };
+		// int[] Ar1 = {2, 3};
+		int[] copyAr = copyArray(Ar);
+
+		toDo(copyAr);
+		// int[] copyAr1 = copyArray(Ar);
+
+	}
+
+	private static int[] copyArray(int[] Sergei) {
+		int[] copiedArray = new int[Sergei.length];
+		for (int i = 0; i < Sergei.length; i++) {
+			copiedArray[i] = Sergei[i];
+		}
+		return copiedArray;
+	}
+
+	private static void toDo(int[] inputArray) {
+		int[] resultArray = copyArray(inputArray);
 
 	}
 
--- src/homework/practice1/taskTwo.java
+++ src/homework/practice1/taskTwo.java
@@ -17,16 +17,35 @@ public class taskTwo {
 		for (int i = 0; i < array.length; i++) {
 			array[i] = inArray.nextInt();
 		}
+
+		copyArray(array);
+		int[] copyAr = copyArray(array);
 		printLine(args);
 		printArray(array);
 		printLine(args);
 		shortestAndLongestNumber(array);
 		printLine(args);
-		averageValue();
+		averageValue(array);
+		printLine(args);
+		evenNumber(array);
+		printLine(args);
+		threeAndFiveNumbers(array);
+		printLine(args);
+		primeNumbers(array);
+		printLine(args);
+		digitsIncreases(array);
+		printLine(args);
 
 	}
 
-	
+	private static int[] copyArray(int[] copyArray) {
+		int[] copiedArray = new int[copyArray.length];
+		for (int i = 0; i < copyArray.length; i++) {
+			copiedArray[i] = copyArray[i];
+		}
+		return copiedArray;
+	}
+
 	private static void printLine(String[] args) {
 		System.out.println("_________________________________________________");
 	}
@@ -78,12 +97,101 @@ public class taskTwo {
 		System.out.println("The length of the shortest number is: " + min + " its value is " + minValue
 				+ " the first entered number is: " + minLengthVal);
 	}
-	
-	private static void averageValue() {
-		
-		
-		
+
+	private static void averageValue(int[] array) {
+		int sumLenght = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			sumLenght += String.valueOf(array[i]).length();
+		}
+		float arrayLenght = (array.length);
+		float arrayAvarage = (sumLenght / arrayLenght);
+		System.out.println("Avarage in array is - " + arrayAvarage);
+		for (int i = 0; i < array.length; i++) {
+			if (String.valueOf(array[i]).length() > arrayAvarage) {
+
+				System.out.println("Number more then avarage - " + array[i] + " It's lenght is - "
+						+ String.valueOf(array[i]).length());
+			} else if (String.valueOf(array[i]).length() < arrayAvarage) {
+
+				System.out.println("Number less then avarage - " + array[i] + " It's lenght is - "
+						+ String.valueOf(array[i]).length());
+			}
+		}
 	}
 
+	private static void evenNumber(int[] array) {
+		boolean nothingFound = true;
+		int qualityOfNumbers = 0;
+		for (int i = 0; i < array.length; i++) {
+			int digitCheck = 0;
+			int localArray = array[i];
+			while (localArray != 0) {
+
+				int evenDigit = (localArray % 10) % 2;
+				if (evenDigit != 0) {
+					digitCheck++;
+					break;
+				}
+				localArray = localArray / 10;
+			}
+			if (digitCheck == 0) {
+				qualityOfNumbers++;
+				nothingFound = false;
+			}
+		}
+
+		if (nothingFound) {
+			System.out.println("There isn't any number with all even digits");
+		} else {
+			System.out.println("The quality of the numbers with all even digits - " + " " + qualityOfNumbers);
+		}
+	}
+
+	private static void threeAndFiveNumbers(int[] array) {
+
+		int[] localArray = copyArray(array);
+		int countOfNonMatchingNumber = 0;
+		for (int i = 0; i < localArray.length; i++) {
+
+			if ((array[i] % 3) == 0 || (array[i] % 5) == 0) {
+				System.out.println("The numbers that divided by 3 or 5 are: " + array[i]);
+			} else {
+				countOfNonMatchingNumber++;
+
+			}
+
+		}
+		if (countOfNonMatchingNumber == localArray.length) {
+			System.out.println("There aren't any numbers that divided by 3 or 5");
+		}
+	}
+
+	private static void primeNumbers(int[] simpleNumbersarray) {
+
+		int[] localArray = copyArray(simpleNumbersarray);
+		int checkForAbsentPrimeNumbers = 0;
+		for (int i = 0; i < localArray.length; i++) {
+			for (int checkPrimeNumber = 2; checkPrimeNumber <= localArray[i]; checkPrimeNumber++) {
+				if ((localArray[i] % checkPrimeNumber) == 0 && (localArray[i] == checkPrimeNumber)) {
+					System.out.println("The prime numbers are: " + localArray[i]);
+				} else {
+					checkForAbsentPrimeNumbers++;
+				}
+			}
+			if (checkForAbsentPrimeNumbers == localArray.length) {
+				System.out.println("There aren't any prime numbers");
+			}
+
+		}
+	}
+
+	private static void digitsIncreases(int[] digitsIncreasesArray) {
+		int checkFordigitsIncreasesNumbers = 0;
+		int[] localArray = copyArray(digitsIncreasesArray);
+		for (int i = 0; i < localArray.length; i++) {
+			
+		}
+	}
 
 }
