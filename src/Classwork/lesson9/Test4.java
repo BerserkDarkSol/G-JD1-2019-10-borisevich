138337b9aee83ee9c89b31dd4a8d7396be361b13
114-10\user <nestor.s@mail.ru>
2019-11-01 21:51:10 +0300

    list

---
+++ src/Classwork/lesson9/Test4.java
@@ -0,0 +1,24 @@
+package classwork.lesson9;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+
+		Set<String> words = new HashSet<String>();
+		words.add("One");
+		words.add("Two");
+		words.add("Three");
+		words.add("Four");
+
+		Iterator<String> iterator = words.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+
+	}
+
+}
