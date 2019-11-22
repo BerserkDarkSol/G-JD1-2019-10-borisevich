5fba47cd853028d0c47588b09e18516e9a783ee7
114-10\user <nestor.s@mail.ru>
2019-11-22 21:59:38 +0300

    classwork 13 Multithreading (monitor)

---
+++ src/Classwork/lesson13_Multithreading/Sample.java
@@ -0,0 +1,10 @@
+package classwork.lesson13_Multithreading;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		Thread currentthread = Thread.currentThread();
+		System.out.println(currentthread);
+	}
+
+}
