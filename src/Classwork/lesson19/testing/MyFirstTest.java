9d9b279aa8a4cda5bac5f4ab408a16556605dedf
114-10\user <nestor.s@mail.ru>
2019-12-13 21:19:45 +0300

    classwork - unit tests

---
+++ src/Classwork/lesson19/testing/MyFirstTest.java
@@ -0,0 +1,24 @@
+package classwork.lesson19.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+	@Test
+	public void testSample() {
+		System.out.println("execute test create");
+	}
+
+	@Test
+	public void testUpdate() {
+		System.out.println("execute test update");
+	}
+
+	@Test
+	public void testDelete() {
+		System.out.println("execute test delete");
+	
+	}
+	
+
+}
